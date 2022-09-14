929701c1d1bb1f477ad590ae9e5aa9309ea0b800
victor <menezes.victorsantos@outlook.com>
2022-09-13 21:55:21 -0300

    err input working

--- app/src/Pages/System/LogIn/index.js
+++ app/src/Pages/System/LogIn/index.js
@@ -4,10 +4,11 @@ import Logo from "../../../assets/images/login-logo.svg";
 import { userLogin } from "../../../api/userApi.js";
 import { useState } from "react";
 export default function Index() {
-	const [classErr, setClassErr] = useState("default-input");
+	const [classErrEmail, setClassErrEmail] = useState("default-input");
+	const [btn, setBtn] = useState(false)
+	const [ClassErrPass, setClassErrPass] = useState("default-input");
 	const [email, setEmail] = useState("");
 	const [pass, setPass] = useState("");
-
 	return (
 		<main className="login-main">
 			<div className="login-main-content">
@@ -21,16 +22,31 @@ export default function Index() {
 				<div className="login-main-content-inputs">
 					<div className="input-main">
 						<p className="input-text">E-mail</p>
-						<input type="email" className={classErr} placeholder="medme@medme.com" value={email} onChange={e => setEmail(e.target.value)} />
+						<input type="email" className={classErrEmail} placeholder="medme@medme.com" value={email} onChange={e => setEmail(e.target.value)} />
+						{classErrEmail === "err-input" && <p className="err-p">Este campo não pode estar vazio.</p>}
 					</div>
 					<div className="input-main">
 						<p className="input-text">Senha</p>
-						<input type="password" className={classErr} placeholder="********" value={pass} onChange={e => setPass(e.target.value)} />
+						<input type="password" className={ClassErrPass} placeholder="********" value={pass} onChange={e => setPass(e.target.value)} />
+						{ClassErrPass === "err-input" && <p className="err-p">Este campo não pode estar vazio.</p>}
 					</div>
 					<button
 						className="sg-lg-btn-complex"
 						onClick={async () => {
 							await userLogin(email, pass);
+							if (!email) {
+								setClassErrEmail("err-input")
+							}
+							else {
+								setClassErrEmail("default-input")
+							}
+
+							if (!pass) {
+								setClassErrPass("err-input")
+							}
+							else {
+								setClassErrPass("default-input")
+							}
 						}}>
 						Entrar
 					</button>
