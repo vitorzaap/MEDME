bd3295e424b5f8b3330350272d1da6fa7ea94251
Julio Boaventura <juliospinola.l@gmail.com>
2022-09-15 20:08:45 -0300

    inputs pré posicionados

--- app/src/Pages/System/Consultas/NovaConsulta/index.js
+++ app/src/Pages/System/Consultas/NovaConsulta/index.js
@@ -10,11 +10,52 @@ export default function Index() {
     }
 
     return(
-        <div className="nova-content">
-                
-                    <h1>pintooo</h1>
-                    <button onClick={hideNova}> pintoo </button>
-               
-        </div>
+        <main className="nova-content">
+            <section>
+                <div  className="title-nova">
+                    <h1>Nova Consulta #3</h1>
+                </div>
+                <div className="main-content-nova">
+                    <div className="l-nova">
+                        <div className="paciente-input">
+                            <label>Paciente</label>
+                            <input />
+                        </div>
+                        <div className="descricao-input">
+                            <label>Descrição</label>
+                            <textarea placeholder="Escreva algo de importante"></textarea>
+                        </div>
+                        <div className="data-input">
+                            <label>Data e hora</label>
+                            <div className="data-hora-inputs">
+                                <input />
+                                <input />
+                            </div>
+                        </div>
+                        <div className="tipo-input">
+                            <label>Tipo de consulta</label>
+                            <input />
+                        </div>
+                    </div>
+                    <div className="r-nova">
+                        <div className="plataforma-input">
+                            <label>Plataforma da consulta</label>
+                            <input />
+                        </div>
+                        <div className="preco-input">
+                            <label>Preço</label>
+                            <p>0</p>
+                        </div>
+                        <div className="link-input">
+                            <label>Link da consulta</label>
+                            <p></p>
+                        </div>
+                    </div>
+                </div>
+                <div className="button-confirm">
+                    <button>Confirmar consulta</button>
+                </div>
+            </section>              
+        </main>
     )
 }
