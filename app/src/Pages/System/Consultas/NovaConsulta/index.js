eda41ed57fb9cce2ec0d574278ee5c8fa8c1a245
victor <menezes.victorsantos@outlook.com>
2022-09-22 10:03:38 -0300

    voltar ne

--- app/src/Pages/System/Consultas/NovaConsulta/index.js
+++ app/src/Pages/System/Consultas/NovaConsulta/index.js
@@ -1,46 +1,74 @@
-import "./index.scss";
-import "../../../Common/common.scss";
-import iconuser from "../../../../assets/images/image 4.svg";
-import { useState } from "react";
+import "./index.scss"
+import "../../../Common/common.scss"
+import iconuser from "../../../../assets/images/image 4.svg"
 
 export default function Index(props) {
-	const [consult, setConsult] = useState(1);
-	function hideNova() {
-		var element = document.getElementById("pop-up");
-		element.classList.remove("show-main");
-	}
+    
+    function hideNova(){
+        var element = document.getElementById("pop-up")
+        element.classList.remove("show-main")
+    }
 
-	return (
-		<main className="consult-content">
-			<div className="title">
-				<h1>Nova Consulta #{consult}</h1>
-			</div>
-			<div className="main-input">
-                <div className="r-input">
-                <div className="input-main">
-						<p className="input-text">Plataforma</p>
-						<textarea cols="10" rows="10" className="default-textarea" placeholder="Escreva algo de importante"></textarea>
-					</div>
+    return(
+        <main  className="nova-content">
+            <section>
+                <div  className="title-nova">
+                    <h1>Nova Consulta #3</h1>
                 </div>
-
-				<div className="l-input">
-					<div className="input-main">
-						<p className="input-text">Plataforma</p>
-						<input type="email" className="default-input" placeholder="Google Meet" />
-					</div>
-					<div className="input-main">
-						<p className="input-text">Preço</p>
-						<input type="number" className="default-input" placeholder="R$100" />
-					</div>
-					<div className="input-main">
-						<p className="input-text">Link</p>
-						<input type="email" className="default-input" placeholder="https://meet.com/suaconsultaonline" />
-					</div>
-				</div>
-			</div>
-			<div className="div-btn">
-				<button className="sg-lg-btn-complex">Confirmar consulta</button>
-			</div>
-		</main>
-	);
+                <div className="main-content-nova">
+                    <div className="l-nova">
+                        <div className="paciente-input">
+                            <label>Paciente</label>
+                            <select>
+                                <option> 
+                                    <img src={iconuser} /> 
+                                    <p>Sr.Piton</p>
+                                </option>
+                            </select>
+                        </div>
+                        <div className="descricao-input">
+                            <label>Descrição</label>
+                            <textarea placeholder="Escreva algo de importante"></textarea>
+                        </div>
+                        <div className="data-input">
+                            <label>Data e hora</label>
+                            <div className="data-hora-inputs">
+                                <input type="date" className="date-input" />
+                                <input type="time" className="time-input" />
+                            </div>
+                        </div>
+                        <div className="tipo-input">
+                            <label>Tipo de consulta</label>
+                            <select>
+                                <option>
+                                <p>Psicologia</p>
+                                </option>
+                            </select>
+                        </div>
+                    </div>
+                   
+                    <div className="r-nova">
+                        <div className="plataforma-input">
+                            <label>Plataforma da consulta</label>
+                            <input  className="plat-in"  placeholder="Escreva a plataforma"></input>
+                        </div>
+                        <div className="preco-input">
+                            <label>Preço</label>
+                            <input type="number" className="pre-in" placeholder="Defina um preço">
+                            </input>
+                           
+                        </div>
+                        <div className="link-input">
+                            <label>Link da consulta</label>
+                            <input className="link-in" placeholder="Cole o link"></input>
+                        </div>
+                    </div>
+                </div>
+                
+                <div className="button-confirm">
+                    <button className="button-consult" onClick={hideNova} >Confirmar consulta</button>
+                </div>
+            </section>              
+        </main>
+    )
 }
