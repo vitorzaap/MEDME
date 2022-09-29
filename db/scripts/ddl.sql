7229fd6158a20ab6b170216ec8db15d4ff6703de
Vitor Santos <102739967+vitorzapp@users.noreply.github.com>
2022-09-29 20:10:20 -0300

    Udpate ddl.sql

--- db/scripts/ddl.sql
+++ db/scripts/ddl.sql
@@ -1,83 +1,95 @@
--- fiz algumas correções. Não garanto 100% de acerto.
+SHOW DATABASES;
 CREATE DATABASE medmedb;
-DROP DATABASE medmedb;
 USE medmedb;
-
+SHOW TABLES;
 
 CREATE TABLE tb_usuario(
-	id_usuario			int primary key auto_increment,
-    nm_usuario			varchar(200) not null,
-    ds_email			varchar(200) not null,
-    ds_senha			varchar(200) not null,
-    img_icon			varchar(200)
+	id_usuario			INT PRIMARY KEY AUTO_INCREMENT,
+    	nm_usuario			VARCHAR(200) NOT NULL,
+    	ds_email			VARCHAR(200) NOT NULL,
+    	ds_senha			VARCHAR(200) NOT NULL,
+    	img_icon			VARCHAR(200)
 );
 
 CREATE TABLE tb_atuacao(
-	id_atuacao			int primary key auto_increment,
-    ds_atuacao			varchar(200) not null
+	id_atuacao			INT PRIMARY KEY AUTO_INCREMENT,
+   	ds_atuacao			VARCHAR(200) NOT NULL
 );
-
-
 
 CREATE TABLE tb_medico(
-	id_medico		int primary key auto_increment,
-    nm_medico		varchar(200) not null,
-    ds_email		varchar(200) not null,
-    ds_senha		varchar(24) not null,
-    ds_medico		varchar(1500),
-    img_icon		varchar(500),
-    id_atuacao1		int not null,
-    id_atuacao2		int,
-	FOREIGN KEY (id_atuacao1) REFERENCES tb_atuacao(id_atuacao),
-    FOREIGN KEY (id_atuacao2) REFERENCES tb_atuacao(id_atuacao)
+	 id_medico		INT PRIMARY KEY AUTO_INCREMENT,
+   	 nm_medico		VARCHAR(200) NOT NULL,
+   	 ds_email		VARCHAR(200) NOT NULL,
+   	 ds_senha		VARCHAR(24) NOT NULL,
+   	 ds_medico		VARCHAR(1500),
+   	 img_icon		VARCHAR(500),
+   	 id_atuacao		INT NOT NULL,
+   	 id_atuacao1		INT,
+   	 nr_consulta		INT DEFAULT 0,
+	 FOREIGN KEY (id_atuacao) REFERENCES tb_atuacao(id_atuacao),
+    	 FOREIGN KEY (id_atuacao1) REFERENCES tb_atuacao(id_atuacao)
 );
 
-CREATE TABLE tb_consulta(
-	id_consulta				int primary key auto_increment,
-    id_medico				int not null,
-    id_usuario				int ,
-    ds_consulta				varchar(2000) not null,
-    dt_consulta				date not null,
-    tm_consulta				time not null,
-    id_atuacao				int ,
-    ds_plataforma			varchar(200) ,
-    vl_preco				decimal not null,
-    ds_link					varchar(500) ,
-    ds_situacao				varchar(200) ,
-    FOREIGN KEY (id_medico) REFERENCES tb_medico(id_medico),
-    FOREIGN KEY (id_usuario) REFERENCES tb_usuario(id_usuario),
-    FOREIGN KEY (id_atuacao) REFERENCES tb_medico(id_atuacao1)
-);
+CREATE TABLE tb_plataforma(
+	id_plataforma		INT PRIMARY KEY AUTO_INCREMENT,
+   	ds_plataforma		VARCHAR(200)
+ );
+ 
+ CREATE TABLE tb_consulta(
+	id_consulta				INT PRIMARY KEY AUTO_INCREMENT,
+   	 id_medico				INT NOT NULL,
+    	id_usuario				INT,
+   	 id_atuacao				INT,
+   	 id_plataforma				INT,
+   	 ds_consulta				VARCHAR(2000) NOT NULL,
+   	 dt_consulta				DATE NOT NULL,
+   	 tm_consulta				TIME NOT NULL,
+   	 vl_preco				DECIMAL(10, 2) NOT NULL,
+   	 ds_link				VARCHAR(500) ,
+   	 ds_situacao				VARCHAR(200) ,
+   	 FOREIGN KEY (id_medico) REFERENCES tb_medico(id_medico),
+   	 FOREIGN KEY (id_usuario) REFERENCES tb_usuario(id_usuario),
+   	 FOREIGN KEY (id_atuacao) REFERENCES tb_atuacao(id_atuacao),
+   	 FOREIGN KEY (id_plataforma) REFERENCES tb_plataforma(id_plataforma)
+ );
 
 CREATE TABLE tb_situacao_consulta(
-	id_situacao_consulta	int primary key auto_increment,
-    id_consulta				int not null,
-    ds_situacao				varchar(50) not null,
-    FOREIGN KEY (id_consulta) REFERENCES tb_consulta(id_consulta)
+	id_situacao_consulta		INT PRIMARY KEY AUTO_INCREMENT,
+    	id_consulta			INT NOT NULL,
+    	ds_situacao			VARCHAR(50) NOT NULL,
+    	FOREIGN KEY (id_consulta) REFERENCES tb_consulta(id_consulta)
 );
 
 CREATE TABLE tb_avaliacao(
-	id_avaliacao 	int primary key auto_increment,
-    id_medico		int not null,
-    id_usuario		int not null,
-    ds_avaliacao	varchar(2000) not null,
-    nr_avaliacao	int not null,
+	id_avaliacao 		INT PRIMARY KEY AUTO_INCREMENT,
+   	id_medico		INT NOT NULL,
+    	id_usuario		INT NOT NULL,
+    	ds_avaliacao		VARCHAR(2000) NOT NULL,
+   	nr_avaliacao		INT NOT NULL,
 	FOREIGN KEY (id_medico) REFERENCES tb_medico(id_medico),
-    FOREIGN KEY (id_usuario) REFERENCES tb_usuario(id_usuario)
+    	FOREIGN KEY (id_usuario) REFERENCES tb_usuario(id_usuario)
 );
 
 CREATE TABLE tb_conversa(
-	id_conversa		int primary key auto_increment,
-    id_medico		int not null,
-    id_usuario		int not null,
-    FOREIGN KEY (id_medico) REFERENCES tb_medico(id_medico),
-    FOREIGN KEY (id_usuario) REFERENCES tb_usuario(id_usuario)
+	id_conversa		INT PRIMARY KEY AUTO_INCREMENT,
+   	id_medico		INT NOT NULL,
+    	id_usuario		INT NOT NULL,
+    	FOREIGN KEY (id_medico) REFERENCES tb_medico(id_medico),
+    	FOREIGN KEY (id_usuario) REFERENCES tb_usuario(id_usuario)
 );
 
 CREATE TABLE tb_mensagem(
-	id_mensagem		int primary key auto_increment,
-    id_conversa		int not null,
-    ds_mensagem		varchar(5000) not null,
-    FOREIGN KEY (id_conversa) REFERENCES tb_conversa(id_conversa)
+	id_mensagem		INT PRIMARY KEY AUTO_INCREMENT,
+    	id_conversa		INT NOT NULL,
+    	ds_mensagem		VARCHAR (5000) NOT NULL,
+    	FOREIGN KEY (id_conversa) REFERENCES tb_conversa(id_conversa)
 );
 
+CREATE TABLE tb_notificacao(
+	id_notificacao		INT PRIMARY KEY AUTO_INCREMENT,
+    	id_mensagem		INT,
+    	id_conversa		INT,
+    	ds_notificacao		VARCHAR(500),
+	FOREIGN KEY (id_mensagem) REFERENCES tb_mensagem(id_mensagem),
+    	FOREIGN KEY (id_conversa) REFERENCES tb_conversa(id_conversa)
+);
