09cf1e2077bf6e8e706c86950ea9c1fd1faed313
Julio Boaventura <juliospinola.l@gmail.com>
2022-10-27 23:42:34 -0300

    feedback === sigin

--- app/src/Pages/System/LandingPage/Feedback/index.js
+++ app/src/Pages/System/LandingPage/Feedback/index.js
@@ -1,54 +1,43 @@
 import "./index.scss";
-import "../../../Common/common.scss"
-import Logo from "../../../../assets/images/Logo.svg"
-import { useState } from "react"
+import "../../../Common/common.scss";
+import Logo from "../../../../assets/images/login-logo.svg";
+
 import { useNavigate } from "react-router-dom"
-import logoFooter from "../../../../assets/images/logo-footer.svg"
 
+export default function Index() {
+	
+	const nav = useNavigate();
 
-export default function Index(){
-    const navigate = useNavigate()
-    return(
-        <main className="feedback">
-            <header>
-                <img src={Logo} className='lm-header-logo' alt="logo"/>
-                <div className="lm-header-div">
-                    <button className="lm-button-1" onClick={() => { navigate("/user/login")}}>Fazer Login</button>
-                </div>
-            </header>
-            <section>
-                <h1>Fale Conosco</h1>
-                <div className="formulario-feedback">
-                    <div>
-                        <label>Nome Completo</label>
-                        <input type="text" />
-                    </div>
-                    <div>
-                        <label>WhatsApp</label>
-                        <input type="text" />
-                    </div>
-                    <div>
-                        <label>Email</label>
-                        <input type="text" />
-                    </div>
-                    <div>
-                        <label>Mensagem</label>
-                        <textarea />
-                    </div>
-                </div>
-                <button>Enviar</button>
-            </section>
-            <footer>
-                <div className="footer-content">
-                    <img src={logoFooter} alt='logo-roxa' />
-                    <div className="links-footer">
-                        <a onClick={() => { navigate("/feedback")}}>Envie um feedback</a>
-                        <a onClick={() => { navigate("/user/login")}}>Termos e privacidade</a>
-                        <a onClick={() => { navigate("/user/login")}}>Trabalhe conosco</a>
-                    </div>
-                    <p className="footer-signature">©2022, Medme Company.</p>
-                </div>
-            </footer>
-        </main>
-    )
+	return (
+		<main className="sigin-main">
+			<div className="sigin-main-content">
+				<div className="sigin-main-content-text">
+					<img src={Logo} alt="Logo" />
+					<h1>Fale Conosco</h1>
+				</div>
+				<div className="sigin-main-content-inputs">
+					<div className="input-main">
+						<p className="input-text">Nome</p>
+						<input type="text" className="default-input"/>
+					</div>
+					<div className="input-main">
+						<p className="input-text">E-mail</p>
+						<input type="text"  className="default-input"/>
+					</div>
+					<div className="input-main">
+						<p className="input-text">WhatsApp</p>
+						<input type="email" className="default-input"/>
+					</div>
+					<div className="input-main">
+						<p className="input-text">Mensagem</p>
+						<textarea className="default-textarea" />
+					</div>
+					<button className="sg-lg-btn">
+						Enviar
+					</button>
+
+				</div>
+			</div>
+		</main>
+	);
 }
