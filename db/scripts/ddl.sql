dc692727984ddc4242cbd0b958c1f697a43851cc
Vitor Santos <102739967+vitorzapp@users.noreply.github.com>
2022-11-11 14:07:54 -0300

    dml and ddl

--- db/scripts/ddl.sql
+++ db/scripts/ddl.sql
@@ -1,55 +1,51 @@
+SHOW DATABASES;
 CREATE DATABASE medmedb;
 USE medmedb;
-
+SET foreign_key_checks = 0; 
 CREATE TABLE tb_usuario(
 		id_usuario			INT PRIMARY KEY AUTO_INCREMENT,
-    	nm_usuario			VARCHAR(10) NOT NULL,
+    	nm_usuario			VARCHAR(50) NOT NULL,
 		sbr_usuario         VARCHAR(200) NOT NULL,
     	ds_email			VARCHAR(200) NOT NULL,
     	ds_senha			VARCHAR(200) NOT NULL,
     	img_icon			VARCHAR(200)
 );
-
 CREATE TABLE tb_atuacao(
 	id_atuacao			INT PRIMARY KEY AUTO_INCREMENT,
    	ds_atuacao			VARCHAR(200) NOT NULL
 );
-
 CREATE TABLE tb_medico(
 	 id_medico			INT PRIMARY KEY AUTO_INCREMENT,
    	 nm_medico			VARCHAR(200) NOT NULL,
    	 ds_email			VARCHAR(200) NOT NULL,
-   	 ds_senha			VARCHAR(24) NOT NULL,
+   	 ds_senha			VARCHAR(200) NOT NULL,
    	 ds_medico			VARCHAR(1500),
-   	 img_icon			VARCHAR(500),
+   	 img_icon			VARCHAR(200),
    	 id_atuacao			INT NOT NULL,
    	 id_atuacao1		INT,
    	 nr_consulta		INT DEFAULT 0,
 	 FOREIGN KEY (id_atuacao) REFERENCES tb_atuacao(id_atuacao),
      FOREIGN KEY (id_atuacao1) REFERENCES tb_atuacao(id_atuacao)
 );
-
 CREATE TABLE tb_plataforma(
 	id_plataforma		INT PRIMARY KEY AUTO_INCREMENT,
    	ds_plataforma		VARCHAR(200)
  );
-
-CREATE TABLE tb_situacao(
+ CREATE TABLE tb_situacao(
 	id_situacao 	INT PRIMARY KEY AUTO_INCREMENT,
-	ds_situacao		VARCHAR(60)
+	ds_situacao		VARCHAR(50)
 );
- 
  CREATE TABLE tb_consulta(
 	 id_consulta				INT PRIMARY KEY AUTO_INCREMENT,
-   	 id_medico					INT NOT NULL,
+   	 id_medico					INT,
      id_usuario					INT,
    	 id_atuacao					INT,
    	 id_plataforma				INT,
-   	 ds_consulta				VARCHAR(2000) NOT NULL,
-   	 dt_consulta				DATE NOT NULL,
-   	 tm_consulta				TIME NOT NULL,
-   	 vl_preco					DECIMAL(10, 2) NOT NULL,
-   	 ds_link					VARCHAR(500) ,
+   	 ds_consulta				VARCHAR(2000),
+   	 dt_consulta				DATE,
+   	 tm_consulta				TIME,
+   	 vl_preco					DECIMAL(10, 2),
+   	 ds_link					VARCHAR(500),
    	 id_situacao				INT,
    	 FOREIGN KEY (id_medico) REFERENCES tb_medico(id_medico),
    	 FOREIGN KEY (id_usuario) REFERENCES tb_usuario(id_usuario),
@@ -57,8 +53,7 @@ CREATE TABLE tb_situacao(
    	 FOREIGN KEY (id_plataforma) REFERENCES tb_plataforma(id_plataforma),
 	 FOREIGN KEY (id_situacao) REFERENCES tb_situacao(id_situacao)
  );
-
-CREATE TABLE tb_avaliacao(
+ CREATE TABLE tb_avaliacao(
 	id_avaliacao 		INT PRIMARY KEY AUTO_INCREMENT,
    	id_medico			INT NOT NULL,
     id_usuario			INT NOT NULL,
@@ -67,20 +62,17 @@ CREATE TABLE tb_avaliacao(
 	FOREIGN KEY (id_medico) REFERENCES tb_medico(id_medico),
     FOREIGN KEY (id_usuario) REFERENCES tb_usuario(id_usuario)
 );
-
 CREATE TABLE tb_sender(
 		id_sender		INT PRIMARY KEY AUTO_INCREMENT,
         nm_sender		VARCHAR(50)
 );
-
 CREATE TABLE tb_conversa(
 	id_conversa		INT PRIMARY KEY AUTO_INCREMENT,
-   	id_medico		INT NOT NULL,
-    id_usuario		INT NOT NULL,
+   	id_medico		INT,
+    id_usuario		INT,
     FOREIGN KEY (id_medico) REFERENCES tb_medico(id_medico),
     FOREIGN KEY (id_usuario) REFERENCES tb_usuario(id_usuario)
 );
-
 CREATE TABLE tb_mensagem(
 	id_mensagem			INT PRIMARY KEY AUTO_INCREMENT,
     id_typeOfSender		INT,
--- db/scripts/dml.sql
+++ db/scripts/dml.sql
@@ -1,21 +1,43 @@
 USE medmedb;
 -- EXECUTAR NA ORDEM!!!
+
+--TB_SENDER
 INSERT INTO tb_sender(nm_sender) EXECUTAR NA ORDEM!!!
 			VALUES("User");
 INSERT INTO tb_sender(nm_sender)
 			VALUES("Doctor");
 INSERT INTO tb_sender(nm_sender)
 			VALUES("System");
--- EXECUTAR NA ORDEM!!!
+--TB_SENDER
 
+-- TB_SITUACAO
+INSERT INTO tb_situacao(ds_situacao)
+			VALUES("Resposta Pendente");
+INSERT INTO tb_situacao(ds_situacao)
+			VALUES("Consulta Aceita");
+INSERT INTO tb_situacao(ds_situacao)
+			VALUES("Consulta Recusada");
+INSERT INTO tb_situacao(ds_situacao)
+			VALUES("Consulta Avaliada");
+-- TB_SITUACAO
 
-INSERT INTO tb_usuario(nm_usuario, sbr_usuario, ds_email, ds_senha)
-        VALUES("Pedro","Horvath Montemurro", "PedroHorvathMontemurro@gmail.com","PedroHorath123");
+-- TB_ATUACAO
 INSERT INTO tb_atuacao(ds_atuacao)
 		VALUES("Dentista");
 INSERT INTO tb_atuacao(ds_atuacao)
 		VALUES("Veterinario");
 INSERT INTO tb_atuacao(ds_atuacao)
 		VALUES("Psicólogo");
-INSERT INTO tb_atuacao(ds_atuacao)
-		VALUES("Terapeuta Sexual");
+-- TB_ATUACAO
+
+-- TB_USUARIO
+INSERT INTO tb_usuario(nm_usuario, sbr_usuario, ds_email, ds_senha)
+        VALUES("Pedro","Horvath Montemurro", "PedroHorvathMontemurro@gmail.com","PedroHorath123");
+-- TB_USUARIO
+
+-- TB_MEDICO
+INSERT INTO tb_medico(nm_medico, ds_email, ds_senha, ds_medico, id_atuacao, id_atuacao1)
+			VALUES("Victor Santos", "victorsantos@medme.com", "medme1234", "Olá, Sou um médico especialista em várias coisas, inicie uma conversa comigo para que possamos marcar uma consulta!", 1, 3);
+-- TB_MEDICO
+
+-- EXECUTAR NA ORDEM!!!
