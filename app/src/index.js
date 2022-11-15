2f6d3a872192a5150439c6d9bbc494f86439e04b
Kalel <ZoroPlay1220@gmail.com>
2022-11-15 19:02:20 -0300

    teste

--- app/src/index.js
+++ app/src/index.js
@@ -5,7 +5,6 @@ import Routes from "./Routes.js"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    
     <Routes />
   </React.StrictMode>
 );
